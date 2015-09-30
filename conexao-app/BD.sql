4dab67f1dae9024aebab89c6010788991e6455bc
Hugo Mesquita <hugomesquitaweb@gmail.com>
2015-09-30 16:36:43 -0400

    Banco de dados

--- conexao-app/BD.sql
+++ conexao-app/BD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: 30-Set-2015 às 22:27
+-- Generation Time: 30-Set-2015 às 22:35
 -- Versão do servidor: 5.5.38
 -- PHP Version: 5.5.18
 
@@ -28,15 +28,6 @@ CREATE TABLE `cliente` (
   `tel` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `cliente`
---
-
-INSERT INTO `cliente` (`id`, `nome`, `cpf`, `rg`, `tel`, `email`) VALUES
-(9, 'Hugo Mesquita', '02312321', '224354653', '(92)981847654', 'hugomesquitaweb@gmail.com'),
-(11, 'Fulano da Silva', '321312', '23123112', '2312312312', 'hugomesquitaweb@gmail.com'),
-(12, 'kaleid', '897878676', '887866', '82102222', 'kalbarba@hotmail.com');
 
 --
 -- Indexes for dumped tables
